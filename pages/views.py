64ca8b922195818b1234721fd9947c39d4a2c480
Angelos Grigorakis <angelosgreg13@gmail.com>
2024-03-15 15:54:41 +0100

    debug

--- pages/views.py
+++ pages/views.py
@@ -5,23 +5,3 @@ from django.shortcuts import render
 def home(request):
     return render(request, "pages/home.html", {})
 # projects/views.py
-
-from projects.models import Project
-
-def project_index(request):
-    projects = Project.objects.all()
-    context = {
-        "projects": projects
-    }
-    return render(request, "projects/project_index.html", context)
-
-# projects/views.py
-
-# ...
-
-def project_detail(request, pk):
-    project = Project.objects.get(pk=pk)
-    context = {
-        "project": project
-    }
-    return render(request, "projects/project_detail.html", context)
