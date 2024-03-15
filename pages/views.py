c2f54ee49da92fba61d15f9323b003f8990c69f0
Angelos Grigorakis <angelosgreg13@gmail.com>
2024-03-15 15:53:24 +0100

    project view

--- pages/views.py
+++ pages/views.py
@@ -4,3 +4,13 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "pages/home.html", {})
+# projects/views.py
+
+from projects.models import Project
+
+def project_index(request):
+    projects = Project.objects.all()
+    context = {
+        "projects": projects
+    }
+    return render(request, "projects/project_index.html", context)
