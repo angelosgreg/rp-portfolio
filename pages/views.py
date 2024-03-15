2bf54ff758bbf7feae6a7faca5ba77e8bf76db93
Angelos Grigorakis <angelosgreg13@gmail.com>
2024-03-15 15:53:45 +0100

    update view

--- pages/views.py
+++ pages/views.py
@@ -14,3 +14,14 @@ def project_index(request):
         "projects": projects
     }
     return render(request, "projects/project_index.html", context)
+
+# projects/views.py
+
+# ...
+
+def project_detail(request, pk):
+    project = Project.objects.get(pk=pk)
+    context = {
+        "project": project
+    }
+    return render(request, "projects/project_detail.html", context)
