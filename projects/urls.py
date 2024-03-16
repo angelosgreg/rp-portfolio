8fff663c302e84fa9b5afd321fd494b0a6c22e9c
Angelos Grigorakis <angelosgreg13@gmail.com>
2024-03-16 15:20:30 +0100

    url update

--- projects/urls.py
+++ projects/urls.py
@@ -4,4 +4,5 @@ from projects import views
 urlpatterns = [
     path("", views.project_index, name="project_index"),
     path("<int:pk>/", views.project_detail, name="project_detail"),
+    path("projects/", include("projects.urls")),
 ]
